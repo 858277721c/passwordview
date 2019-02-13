b1df992917f9839576ccb1809881e40a94862586
zjun <565061763@qq.com>
2019-02-13 11:02:03 +0800

    demo

--- app/src/main/java/com/sd/passwordview/MainActivity.java
+++ app/src/main/java/com/sd/passwordview/MainActivity.java
@@ -5,15 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
-    private FPasswordView mPasswordView;
+    private FPasswordView view_password_1, view_password_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mPasswordView = findViewById(R.id.view_password);
+        view_password_1 = findViewById(R.id.view_password_1);
+        view_password_2 = findViewById(R.id.view_password_2);
 
-        mPasswordView.setItemBackgroundResource(R.drawable.bg_item);
+        view_password_1.setItemBackgroundResource(R.drawable.bg_item_white);
+        view_password_2.setItemBackgroundResource(R.drawable.bg_item_stroke);
     }
 }
